7efc85824ea03d2f6457b1416a88a9c61518cb52
XyPis <173083847@qq.com>
2016-09-19 17:39:15 +0800

    Init commit

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            ClassLibrary1.Class1 c1 = new ClassLibrary1.Class1();
+            string s = "dragon luo";
+
+            Assert.AreEqual(s.ToUpper(), c1.Upper(s));
+        }
+    }
+}
